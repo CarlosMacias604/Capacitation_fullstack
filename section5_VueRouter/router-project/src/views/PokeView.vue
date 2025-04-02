72ba99c9811a72647320d29cd1648c6bed5eb50f
CarlosMacias604 <carlos.macias@cceo.com.mx>
2025-04-02 11:13:40 -0600

    Feat :sparkles: (section5_VueRouter): Complete the section 5,6,7 of the course of Vue

--- section5_VueRouter/router-project/src/views/PokeView.vue
+++ section5_VueRouter/router-project/src/views/PokeView.vue
@@ -2,11 +2,14 @@
 import axios from "axios";
 import { useRoute, useRouter} from "vue-router";
 import { ref } from "vue";
+import { useFavoritosStore } from "@/store/favoritos";
 
+const useFavoritos = useFavoritosStore();
 const route = useRoute();
 const router = useRouter();
-
 const poke = ref([]);
+
+const {add, findPoke} = useFavoritos;
 
 const back = () =>{
     router.push('/pokemons')
@@ -23,12 +26,14 @@ const getData = async () =>{
     }
 };
 getData();
+
 </script>
 
 <template>
     <div v-if="poke">
         <img :src="poke.sprites?.front_default" alt="pokemon">
         <h1>Poke name: {{ $route.params.name }}</h1>
+        <button :disabled="findPoke(poke.name)" @click="add(poke)" class="btn btn-outline-primary mb-3">Agregar Favoritos</button>
     </div>
     <h1 v-else>Pokemon no encontrado</h1>
     <button class="btn btn-outline-primary" @click="back">Volver</button>
