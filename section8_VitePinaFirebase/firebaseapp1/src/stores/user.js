055344093e3e5819db98fbec9494dd6c87c950d2
CarlosMacias604 <carlos.macias@cceo.com.mx>
2025-04-07 12:29:15 -0600

    Feat :sparkles: (section8_VitePinaFirebase): Complete the section 9 of the course of Vue

--- section8_VitePinaFirebase/firebaseapp1/src/stores/user.js
+++ section8_VitePinaFirebase/firebaseapp1/src/stores/user.js
@@ -1,67 +1,82 @@
-import {defineStore} from 'pinia'
-import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
-import { auth } from '../firebaseConfig'
-import router from '../router';
+import { defineStore } from "pinia";
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    sendEmailVerification,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import router from "../router";
+import { useDatabaseStore } from "./database";
 
-export const useUserStore = defineStore('userStore', {
+export const useUserStore = defineStore("userStore", {
     state: () => ({
         userData: null,
         loadingUser: false,
-        loaddigSession: false
+        loadingSession: false,
     }),
     actions: {
-        async registerUser(email, password){
+        async registerUser(email, password) {
             this.loadingUser = true;
-            try{
-                const {user} = await createUserWithEmailAndPassword(auth, email, password);
-                this.userData = {email: user.email, uid: user.uid}
-                router.push('/');
-            }catch (error){
-                console.log(error)
-            }finally{
+            try {
+                await createUserWithEmailAndPassword(auth, email, password);
+                // this.userData = { email: user.email, uid: user.uid };
+                await sendEmailVerification(auth.currentUser);
+                router.push("/login");
+            } catch (error) {
+                console.log(error.code);
+                return error.code;
+            } finally {
                 this.loadingUser = false;
             }
         },
         async loginUser(email, password) {
             this.loadingUser = true;
-            try{
-                const {user} = await signInWithEmailAndPassword(auth, email, password)
-                this.userData = {email: user.email, uid: user.uid}
-                router.push('/');
-            }catch(error){
-                console.log(error)
-            }finally{
+            try {
+                const { user } = await signInWithEmailAndPassword(
+                    auth,
+                    email,
+                    password
+                );
+                this.userData = { email: user.email, uid: user.uid };
+                router.push("/");
+            } catch (error) {
+                console.log(error.code);
+                return error.code;
+            } finally {
                 this.loadingUser = false;
             }
         },
-        async logoutUser(){
-            this.loadingUser = true;
+        async logoutUser() {
+            const databaseStore = useDatabaseStore();
+            databaseStore.$reset();
             try {
-                await signOut(auth)
+                await signOut(auth);
                 this.userData = null;
-                router.push('/login');
+                router.push("/login");
             } catch (error) {
-                console.log(error)
-            }finally{
-                this.loadingUser = false;
+                console.log(error);
             }
         },
         currentUser() {
             return new Promise((resolve, reject) => {
-                onAuthStateChanged(
+                const unsuscribe = onAuthStateChanged(
                     auth,
                     (user) => {
-                        if(user){
+                        if (user) {
                             this.userData = {
-                                email: user.email, 
+                                email: user.email,
                                 uid: user.uid,
                             };
-                        }else{
+                        } else {
                             this.userData = null;
+                            const databaseStore = useDatabaseStore();
+                            databaseStore.$reset();
                         }
                         resolve(user);
-                    }, 
-                    e => reject(e)
+                    },
+                    (e) => reject(e)
                 );
             });
         },
